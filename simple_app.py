bbd9804ec2277bc7f1ce92d99189adb94af05ea0
manishtailor106 <85630394+manishtailor106@users.noreply.github.com>
2024-03-17 11:17:19 +0530

    Add files via upload

---
+++ simple_app.py
@@ -0,0 +1,16 @@
+#pip install kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class root(App):
+    def build(self): 
+        la=BoxLayout(orientation='vertical')
+        lb=Label(text="Hello There")
+        la.add_widget(lb)
+        return la
+
+if __name__=="__main__":
+    root().run()
+
